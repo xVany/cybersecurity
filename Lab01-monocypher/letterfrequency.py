2dd35a329096590524b2c62b15026f0c9d85b46a
xVany <rayy12204@gmail.com>
2021-09-28 15:31:06 -0400

    updated letterfrequency

--- Lab01-monocypher/letterfrequency.py
+++ Lab01-monocypher/letterfrequency.py
@@ -1,4 +1,20 @@
+import sys
+file = ''
+if len(sys.argv) > 1:
+   file = sys.argv[1]
+   text = open(file).read()
+else:
+   print("error provide a filename")
 
+alpha = "abcdefghijklmnopqrstuvwxyz"
+total = 0
+letters = {}
+for letter in alpha:
+    letters[letter] = 0
 
-with open(arg) as f:
-    contents = f.read()
+for char in text.lower():
+    if char in alpha:
+        letters[char] += 1
+        total += 1
+for char, count in letters.items():
+    print(char, ":", count/total)
