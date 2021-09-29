be3bfee89aaafca5e9aefa91bf01c92fc1a8e66b
Raymond Yeung <43830708+xVany@users.noreply.github.com>
2021-09-29 15:37:56 -0400

    Update cipherDecoder.py

--- Lab01-monocypher/cipherDecoder.py
+++ Lab01-monocypher/cipherDecoder.py
@@ -1,12 +1,33 @@
 import sys
 import letterfrequency
+import math
+
 file = ''
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
    file = sys.argv[1]
    text = open(file).read()
+   cipher = sys.argv[2]
+   ctext open(cipher).read()
 else:
    print("error provide a filename")
 
 alpha = "abcdefghijklmnopqrstuvwxyz"
 
+englishFreq = letterfrequency(file).letters
 
+def highestFreqletter(text):
+   highestFreq = 0
+   tempLetter = ''
+   for letter, freq in letterfrequency(text).items():
+      if freq > highestFreq:
+         tempLetter = letter
+   return tempLetter
+
+x = alpha.find(highestFreqLetter(ctext)) #4
+y = alpha.find(highestFreqLetter(file)) #5
+
+def shiftString(
+
+shift = x - y
+
+if shift < 0:
